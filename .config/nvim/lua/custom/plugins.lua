8d40b647f156cfa59de881b3f55f9548d9f461cf
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-02-26 20:17:52 +0000

    added codium

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,16 @@
 local plugins = {
   {
+    "Exafunction/codeium.vim",
+    event = 'BufEnter',
+    config = function ()
+      vim.g.codeium_disable_bindings = true
+      vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end,
+  },
+  {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
