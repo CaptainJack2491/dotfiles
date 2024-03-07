82405139068f3adf6bc05e3f3f65fd7d98e0d90a
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-03-07 20:42:36 +0000

    7 march 2024

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -37,18 +37,18 @@ local plugins = {
         },
       },
     },
-    picker = {
-      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-      name = "telescope.nvim",
-      -- Optional, configure key mappings for the picker. These are the defaults.
-      -- Not all pickers support all mappings.
-      mappings = {
-        -- Create a new note from your query.
-        new = "<C-x>",
-        -- Insert a link to the selected note.
-        insert_link = "<C-l>",
-      },
-    },
+    -- picker = {
+    --   -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    --   name = "telescope.nvim",
+    --   -- Optional, configure key mappings for the picker. These are the defaults.
+    --   -- Not all pickers support all mappings.
+    --   mappings = {
+    --     -- Create a new note from your query.
+    --     new = "<C-x>",
+    --     -- Insert a link to the selected note.
+    --     insert_link = "<C-l>",
+    --   },
+    -- },
 
       -- see below for full list of options 👇
     },
