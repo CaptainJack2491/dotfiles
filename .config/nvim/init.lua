0753ada28dd9c372dccc7b4771aacb7d18467819
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-09-02 14:54:52 +0100

    added nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.wo.number = true
+vim.wo.signcolumn = "number"
+vim.wo.relativenumber = true
+
+vim.opt.conceallevel = 1
+
+local opts = {}
+vim.g.mapleader = ' '
+
+require("vim-options")
+require("lazy").setup("plugins")
+
+
+
+
+
+
+
+
+
+
