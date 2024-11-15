322e29b48b680c1ec1790e8c427d566667e02df8
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-11-15 02:38:27 +0000

    new script works

--- .config/hypr/scripts/toggle_wallpaper.py
+++ .config/hypr/scripts/toggle_wallpaper.py
@@ -15,8 +15,14 @@ def get_next_wallpaper(current_wallpaper:str, files:list[str]) -> str:
     return files[0]
     
 def change_wallpaper(wallpaper_path:str) -> None:
+    print("changing wallpaper")
+    subprocess.run(["hyprctl", "hyprpaper", "unload", "all"])
     subprocess.run(["hyprctl", "hyprpaper", "preload", wallpaper_path])
     subprocess.run(["hyprctl", "hyprpaper", "wallpaper", ",", wallpaper_path])
+
+
+def generate_colorscheme(wallpaper) -> None:
+    subprocess.run(["wal", "-i", wallpaper, "-n"])
 
 active_wal = subprocess.run(["hyprctl", "hyprpaper", "listactive"], capture_output=True, text=True)
 activewal = active_wal.stdout.strip()
@@ -26,3 +32,4 @@ print(activewal)
 next_wallpaper = get_next_wallpaper(activewal,wallpaper_files)
 print(next_wallpaper)
 change_wallpaper(wallpaper_dir+next_wallpaper)
+generate_colorscheme(wallpaper_dir+next_wallpaper)
