0faece4762946c810c6ab384e1b47145494ce69b
CaptainJack2491 <jayrupnakawala@gmail.com>
2025-11-04 14:17:15 +0000

    updated nvim configs to v0.11

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -21,46 +21,42 @@ return {
     opts = {
       auto_install = true,
     },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "rust_analyzer",
-        }
-      })
-    end,
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
+      -- This part for cmp is still needed and correct
       require'cmp'.setup {
         sources = {
           { name = 'nvim_lsp' }
         }
       }
+
+      -- Get capabilities from cmp_nvim_lsp
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-
-      lspconfig.gopls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tinymist.setup({
-        capabilities = capabilities
-      })
-      lspconfig.pyright.setup({
+      -- NEW WAY: Set global defaults for all LSPs
+      -- This applies your capabilities to every server
+      vim.lsp.config('*', {
         capabilities = capabilities
       })
 
-      lspconfig.gdscript.setup({
-        capabilities = capabilities
-      })
+      -- NEW WAY: Enable the servers you want to use
+      -- nvim-lspconfig provides the default settings,
+      -- and mason-lspconfig will auto-install them.
+      local servers = {
+        "lua_ls",
+        "gopls",
+        "tinymist",
+        "pyright",
+        "gdscript",
+        -- "rust_analyzer", -- I added this from your mason-lspconfig
+      }
 
+      vim.lsp.enable(servers)
+
+      -- Your keymaps are perfect and do not need to change
       vim.keymap.set('n', 'K', vim.lsp.buf.hover)
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,34 +1,13 @@
 return {
   -- The main plugin for Rust integration
   {
-    "simrat39/rust-tools.nvim",
-    config = function()
-      local rt = require("rust-tools")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-      rt.setup({
-        server = {
-          -- Pass capabilities to the rust-analyzer server.
-          capabilities = capabilities,
-          on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-          end,
-        },
-        -- DAP configuration for debugging
-        dap = {
-          adapter = require('rust-tools.dap').get_codelldb_adapter(
-            vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-            vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
-            )
-        },
-      })
-    end,
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = "rust",    -- Lazily load on Rust filetypes
+    -- No config function is needed for a default setup!
   },
 
-  -- Helper for managing dependencies in Cargo.toml
+  -- Helper for managing dependencies in Cargo.toml (This is good, keep it)
   {
     "Saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
@@ -37,6 +16,6 @@ return {
     end,
   },
 
-  -- Debugging engine
+  -- Debugging engine (This is good, keep it)
   { "mfussenegger/nvim-dap" },
 }
